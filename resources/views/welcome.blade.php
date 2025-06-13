d5700409c716f0dc3bcc15c78e15c922b44f9789
JulioNoteHuawei <julioignaciob@gmail.com>
2025-06-12 23:17:56 -0400

    arreglos visuales

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <div class="mt-10 text-center">
         <h1 class="text-3xl font-bold text-green-700">Bienvenido a VolleyTable</h1>
-        <p class="mt-4 text-gray-700">Organiza y gestiona tus partidos de vóleibol fácilmente.</p>
+        <p class="mt-4 text-black">Organiza y gestiona tus partidos de vóleibol fácilmente.</p>
 
         <div class="max-w-md mx-auto mt-8">
             @livewire('partido-rapido-card')
